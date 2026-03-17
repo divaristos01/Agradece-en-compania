a4f7738e4bf64030902a1400bccbe6b615fdd03e
divaristos01 <diegoevaristosantiago.guadalupe@alumnado.fundacionloyola.net>
2026-03-17 23:06:14 +0100

    Add files via upload

---
+++ copia.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INICIO DE SESIÓN</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <header>
+        <h1>AGRADECE EN COMPAÑÍA</h1>
+    </header>
+    <main>
+        <form action="">
+            
+        </form>
+    </main>
+
+
+    <php?  /*como se llama la variable del id del alumno*/
+        <option value="1">TIPO</option>
+    ?>
+
+</body>
+</html>
